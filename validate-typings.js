4d45045d0ba0b74333b56ae426613d419fcab85e
Sainan <63328889+Sainan@users.noreply.github.com>
2025-08-16 00:06:43 +0200

    chore: rewrite validate-typings in node

---
+++ validate-typings.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const indexTypes = fs.readFileSync('index.d.ts', 'utf8');
+let types = indexTypes + '\nexport interface IPublicExportPlus {';
+for (const line of indexTypes.split(/\r?\n/)) {
+    if (line.startsWith('export declare const')) {
+        types += '\n' + line.slice(21);
+    }
+}
+types += '\n}';
+
+console.log('🕐 Generating schemas...');
+fs.writeFileSync('tmp-types.ts', types);
+execSync('npx ts-to-zod tmp-types.ts tmp-schemas.ts --skipValidation', { stdio: 'inherit' });
+
+const schemas = fs
+    .readFileSync('tmp-schemas.ts', 'utf8')
+    .replace(/z\.object/g, 'z.strictObject')
+    .replace('iAbilitySchema = z.strictObject({', 'iAbilitySchema = z.object({');
+fs.writeFileSync('tmp-schemas.ts', schemas);
+
+console.log('🕐 Validating schemas...');
+const test = `import { z } from "zod";
+import { iPublicExportPlusSchema } from "./tmp-schemas";
+import publicExportPlus from "./index";
+try {
+    iPublicExportPlusSchema.parse(publicExportPlus);
+    console.log("✅ Schemas validated successfully.");
+} catch (e) {
+    console.log((e as Error).message);
+}`;
+fs.writeFileSync('tmp-test.ts', test);
+execSync("npx ts-node -O '{\"module\":\"commonjs\"}' tmp-test.ts", { stdio: 'inherit' });
+
+fs.unlinkSync('tmp-schemas.ts');
+fs.unlinkSync('tmp-types.ts');
+fs.unlinkSync('tmp-test.ts');
